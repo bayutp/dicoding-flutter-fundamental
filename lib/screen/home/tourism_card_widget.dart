def374556e128b963f98f539618f1f05af96caec
bayu <bayuteguhpamuji@gmail.com>
2025-08-02 12:03:01 +0700

    modify tourism card widget with constraints box & expanded

--- lib/screen/home/tourism_card_widget.dart
+++ lib/screen/home/tourism_card_widget.dart
@@ -13,17 +13,54 @@ class TourismCard extends StatelessWidget {
       onTap: onTap,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+        child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
           children: [
-            Text(tourism.name, style: const TextStyle(fontSize: 16)),
-            Text(
-              tourism.description,
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-              softWrap: true,
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight: 100,
+                maxWidth: 150,
+                minHeight: 100,
+                minWidth: 150,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.only(
+                  topLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                ),
+                child: Image.network(tourism.image, fit: BoxFit.cover),
+              ),
+            ),
+            const SizedBox.square(dimension: 8),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(tourism.name, style: const TextStyle(fontSize: 16)),
+                  const SizedBox.square(dimension: 6),
+                  Row(
+                    children: [
+                      Icon(Icons.pin_drop_sharp, color: Colors.lightBlue),
+                      Expanded(
+                        child: Text(
+                          tourism.address,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox.square(dimension: 6),
+                  Row(
+                    children: [
+                      Icon(Icons.favorite, color: Colors.red),
+                      Expanded(child: Text(tourism.like.toString())),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
