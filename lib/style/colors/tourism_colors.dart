9883b72fb381b9f4681a7541236c929ddcb7bed1
bayu <bayuteguhpamuji@gmail.com>
2025-08-04 14:26:50 +0700

    add app themes

---
+++ lib/style/colors/tourism_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+enum TourismColors {
+  blue("Blue", Colors.blue);
+
+  const TourismColors(this.name, this.colors);
+
+  final String name;
+  final Color colors;
+}
