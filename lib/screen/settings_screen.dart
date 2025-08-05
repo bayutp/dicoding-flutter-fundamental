9fa1f5388ba716a0bbecdb4e924701df72bc2e6c
bayu <bayuteguhpamuji@gmail.com>
2025-08-05 10:04:05 +0700

    add alert dialog for logout

--- lib/screen/settings_screen.dart
+++ lib/screen/settings_screen.dart
@@ -8,11 +8,32 @@ class SettingsScreen extends StatelessWidget {
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(middle: Text('Settings')),
       child: Center(
-        child: Text(
-          'Dicoding Indonesia',
-          style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+        child: CupertinoButton.filled(
+          child: Text('Logout'),
+          onPressed: () => _openAlert(context),
         ),
       ),
+    );
+  }
+
+  void _openAlert(BuildContext context) {
+    showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Are you sure to Logout?'),
+          actions: [
+            CupertinoDialogAction(
+              child: const Text('No'),
+              onPressed: () => Navigator.pop(context),
+            ),
+            CupertinoDialogAction(
+              child: const Text('Yes'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
     );
   }
 }
