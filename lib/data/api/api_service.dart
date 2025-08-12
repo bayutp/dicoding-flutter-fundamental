0eea1db4bbd1a1f32524a80e391b569838e6bb0d
bayu <bayuteguhpamuji@gmail.com>
2025-08-12 09:12:30 +0700

    add method to fetch detail tourism

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:tourism_app/data/model/tourism_detail_response.dart';
 import 'package:tourism_app/data/model/tourism_list_response.dart';
 
 class ApiService {
@@ -13,6 +14,16 @@ class ApiService {
       return TourismListResponse.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load tourism list');
+    }
+  }
+
+  // fetch data tourism detail
+  Future<TourismDetailResponse> getTourismDetail(int id) async {
+    final response = await http.get(Uri.parse('$_baseUrl/detail/$id'));
+    if (response.statusCode == 200) {
+      return TourismDetailResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load tourism detail');
     }
   }
 }
