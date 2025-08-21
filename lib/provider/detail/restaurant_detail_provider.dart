8eef0a4e3ddcdeb3340825fd1912cc958c741e47
bayu <bayuteguhpamuji@gmail.com>
2025-08-21 21:35:13 +0700

    setup restaurant api provider

---
+++ lib/provider/detail/restaurant_detail_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tourism_app/data/api/api_service.dart';
+import 'package:tourism_app/static/restaurant_detail_result_state.dart';
+
+class RestaurantDetailProvider extends ChangeNotifier {
+  final ApiService _apiService;
+
+  RestaurantDetailProvider(this._apiService);
+
+  RestaurantDetailResultState _resultState = RestaurantDetailNoneState();
+  RestaurantDetailResultState get resultState => _resultState;
+
+  Future<void> getRestaurantDetail(int id) async {
+    try {
+      _emit(RestaurantDetailLoadingState());
+
+      final result = await _apiService.getRestaurantDetail(id);
+      if (result.error) {
+        _emit(RestaurantDetailErrorState(result.message));
+      } else {
+        _emit(RestaurantDetailLoadedState(result.restaurant));
+      }
+    } on Exception catch (e) {
+      _emit(RestaurantDetailErrorState(e.toString()));
+    }
+  }
+
+  void _emit(RestaurantDetailResultState state) {
+    _resultState = state;
+    notifyListeners();
+  }
+}
--- lib/provider/home/restaurant_list_provider.dart
+++ lib/provider/home/restaurant_list_provider.dart
@@ -1,4 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:tourism_app/data/api/api_service.dart';
+import 'package:tourism_app/static/restaurant_list_result_state.dart';
 
-class RestaurantListProvider extends ChangeNotifier{
+class RestaurantListProvider extends ChangeNotifier {
+  final ApiService _apiService;
+
+  RestaurantListProvider(this._apiService);
+
+  RestaurantListResultState _resultState = RestaurantListNoneState();
+
+  RestaurantListResultState get resultState => _resultState;
+
+  Future<void> fetchRestaurantList() async {
+    try {
+      _emit(RestaurantListLoadingState());
+
+      final result = await _apiService.getRestaurantList();
+      if (result.error) {
+        _emit(RestaurantListErrorState(result.message));
+      } else {
+        _emit(RestaurantListLoadedState(result.restaurants));
+      }
+    } on Exception catch (e) {
+      _emit(RestaurantListErrorState(e.toString()));
+    }
+  }
+
+  void _emit(RestaurantListResultState state) {
+    _resultState = state;
+    notifyListeners();
+  }
 }
