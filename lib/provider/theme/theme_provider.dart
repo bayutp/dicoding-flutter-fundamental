9ead5513cfdcaea1e50ff045a5910f7bf1c04269
bayu <bayuteguhpamuji@gmail.com>
2025-09-09 08:48:41 +0700

    apply shared preferences in theme provider

--- lib/provider/theme/theme_provider.dart
+++ lib/provider/theme/theme_provider.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/widgets.dart';
+import 'package:tourism_app/data/model/setting_theme.dart';
+import 'package:tourism_app/service/shared_preferences_service.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  bool _isDark = false;
+  final SharedPreferencesService _service;
 
-  bool get isDark => _isDark;
-  set setDarkMode(bool value) {
-    _isDark = value;
+  ThemeProvider(this._service);
+
+  String _messaage = "";
+  String get message => _messaage;
+
+  SettingTheme? _settingTheme;
+  SettingTheme? get settingTheme => _settingTheme;
+
+  Future<void> setTheme(SettingTheme theme) async {
+    try {
+      await _service.setTheme(theme);
+      _messaage = "New theme applied";
+    } catch (e) {
+      _messaage = "Failed to change theme";
+    }
+    notifyListeners();
+  }
+
+  void getTheme() async {
+    try {
+      _settingTheme = _service.getTheme();
+      _messaage = "Theme loaded successfully";
+    } catch (e) {
+      _messaage = "Failed to retrieve current theme";
+    }
     notifyListeners();
   }
 }
