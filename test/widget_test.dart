ce0c65b6bab26fdd173ff55ad8ca4a3f3767ba83
bayu <bayuteguhpamuji@gmail.com>
2025-09-12 15:04:01 +0700

    add widget testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,32 +1,78 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:mocktail_image_network/mocktail_image_network.dart';
+import 'package:provider/provider.dart';
+import 'package:tourism_app/data/api/api_service.dart';
+import 'package:tourism_app/data/model/restaurant.dart';
+import 'package:tourism_app/data/model/restaurant_list_response.dart';
+import 'package:tourism_app/provider/home/restaurant_list_provider.dart';
+import 'package:tourism_app/provider/main/index_nav_provider.dart';
 
-import 'package:tourism_app/main.dart';
-import 'package:tourism_app/static/navigation_route.dart';
+import 'package:tourism_app/screen/main/main_screen.dart';
+
+import 'provider/home/mock_api.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    final String route = NavigationRoute.mainRoute.name;
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(initialRoute: route));
+  late MockApiService mockApiService;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setUp(() {
+    mockApiService = MockApiService();
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  testWidgets('Check every component on first look of MyApp', (
+    WidgetTester tester,
+  ) async {
+    when(() => mockApiService.getRestaurantList()).thenAnswer(
+      (_) async => RestaurantListResponse(
+        error: false,
+        message: "success",
+        count: 1,
+        restaurants: [
+          Restaurant(
+            id: "rqdv5juczeskfw1e867",
+            name: "Melting Pot",
+            description: "Test desc",
+            pictureId: "14",
+            city: "Gorontalo",
+            rating: 4,
+          ),
+        ],
+      ),
+    );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await mockNetworkImages(() async {
+      await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            Provider<ApiService>(create: (_) => mockApiService),
+            ChangeNotifierProvider(create: (context) => IndexNavProvider()),
+            ChangeNotifierProvider(
+              create: (context) =>
+                  RestaurantListProvider(context.read<ApiService>()),
+            ),
+          ],
+          child: const MaterialApp(home: MainScreen()),
+        ),
+      );
+
+      // checking app bar component
+      expect(find.byType(AppBar), findsOneWidget);
+      expect(find.text("Restaurant App"), findsOneWidget);
+
+      // checking bottom nav bar component
+      expect(find.byType(BottomNavigationBar), findsOneWidget);
+      expect(find.byIcon(Icons.home_rounded), findsOneWidget);
+      expect(find.byIcon(Icons.search_rounded), findsOneWidget);
+      expect(find.byIcon(Icons.favorite_rounded), findsOneWidget);
+      expect(find.byIcon(Icons.settings), findsOneWidget);
+
+      // checking list view component
+      await tester.pumpAndSettle();
+      expect(find.byType(ListView), findsOneWidget);
+      expect(find.text("Melting Pot"), findsOneWidget);
+
+      verify(() => mockApiService.getRestaurantList()).called(1);
+    });
   });
 }
